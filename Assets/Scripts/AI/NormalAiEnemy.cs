3672dc2b58bd8d6d664d0d138efd278524563b81
miguel9177 <rodrigue3@coventry.ac.uk>
2022-04-21 03:48:03 +0100

    Made Ai that takes damage, and drawed a player and a weapon

--- Assets/Scripts/AI/NormalAiEnemy.cs
+++ Assets/Scripts/AI/NormalAiEnemy.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Photon.Pun;
+using TMPro;
 [System.Serializable]
 struct AiEnemy
 {
@@ -12,18 +13,51 @@ struct AiEnemy
     public float moneyWorth;
 }
 
-public class NormalAiEnemy : MonoBehaviour
+public class NormalAiEnemy : MonoBehaviourPun
 {
+    [SerializeField]
+    TextMeshProUGUI tempHp;
+
     [SerializeField]
     //this will store the settings of this enemy
     AiEnemy thisEnemy;
 
+    //this will check if the die function have already been called
+    bool haveIHaverBeenCalled = false;
+
+    //if collided with some collider
     void OnCollisionEnter2D(Collision2D col)
     {
         //this will checl of ythe bullet has a bullet information, if it has it will get damaged
         if(col.gameObject.GetComponent<BulletInformation>())
         {
+            //this will decrease the ai hp
             thisEnemy.hp = thisEnemy.hp - col.gameObject.GetComponent<BulletInformation>().damage;
+            
+            //if the hp is less or equal to 0, call the function die
+            if(thisEnemy.hp <= 0)
+            {
+                //if the fucntion die havent been called on this ai enemy, call the function die
+                if(haveIHaverBeenCalled == false)
+                    //this will call the function Die, on every pc on the server
+                    photonView.RPC("Die", RpcTarget.All);
+
+                //tell the code that the function die has already been called
+                haveIHaverBeenCalled=true;
+            }
         }
+    }
+
+    
+    [PunRPC]
+    void Die()
+    {
+        //destroy this object, since it died
+        Destroy(this.gameObject);
+    }
+
+    private void Update()
+    {
+        tempHp.text = thisEnemy.hp.ToString();
     }
 }
