f40c8f453dd9fb3dd2604137df343d83cb689f55
miguel9177 <rodrigue3@coventry.ac.uk>
2022-04-27 04:37:18 +0100

    added woman player, and different spawn positions

--- Assets/Scripts/OnlineServer/PlayerSpawner.cs
+++ Assets/Scripts/OnlineServer/PlayerSpawner.cs
@@ -7,9 +7,17 @@ using TMPro;
 public class PlayerSpawner : MonoBehaviourPun
 {
     //this will store wich player this script will spawn
-    [SerializeField] private GameObject playerPrefab = null;
+    [SerializeField] private GameObject playerPrefabMan = null;
+    //this will store the woman player that will be spawned
+    [SerializeField] private GameObject playerPrefabWoman = null;
     //this will get access to the free look cinemachine camera component, so that we can say to it wich player to follow on, the void start
     [SerializeField] private CameraFollow playerCameraFollow = null;
+
+    [SerializeField]
+    Transform spawnPoint1;
+
+    [SerializeField]
+    Transform spawnPoint2;
 
     [SerializeField]
     //this will store the player money text to be able to send it to the player gameobject
@@ -17,9 +25,26 @@ public class PlayerSpawner : MonoBehaviourPun
 
     private void Start()
     {
-        //Spawn "online" the player prefab, and assign the photon view to us, with the instatiate keyword this object will be assigned to us, and we are the ones who can move it
-        //if we do the if photonView.IsMine, and will spawn it on 000 positionm and 000 rotation
-        GameObject player = PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
+
+        GameObject player;
+
+        //If the player is the master client spawn at spawn 1
+        if (PhotonNetwork.IsMasterClient)
+        {
+            //Spawn "online" the player prefab, and assign the photon view to us, with the instatiate keyword this object will be assigned to us, and we are the ones who can move it
+            //if we do the if photonView.IsMine, and will spawn it on 000 positionm and 000 rotation
+            player = PhotonNetwork.Instantiate(playerPrefabMan.name, spawnPoint1.position, Quaternion.identity);
+        }   
+        //If the player isnt the master client spawn at spawn 2
+        else
+        {
+            //Spawn "online" the player prefab, and assign the photon view to us, with the instatiate keyword this object will be assigned to us, and we are the ones who can move it
+            //if we do the if photonView.IsMine, and will spawn it on 000 positionm and 000 rotation
+            player = PhotonNetwork.Instantiate(playerPrefabWoman.name, spawnPoint2.position, Quaternion.identity);
+        }
+        
+
+       
 
         //asssign the money textbox to the player statas and functionalities script
         player.GetComponent<PlayerStatsAndFunctionalities>().moneyText = playerMoneyText;
